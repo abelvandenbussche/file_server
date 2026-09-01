f3dcf72631b63e5c5cb7ab76bb56f508386b422f
abelvandenbussche <abel.vandenbussche@gmail.com>
2026-09-01 20:34:44 +0200

    Add simple script injecting and event source

--- src/main.rs
+++ src/main.rs
@@ -43,16 +43,38 @@ fn handle_connection(stream: TcpStream) -> Result<(), Box<dyn Error>> {
         let path = PathBuf::from(parts[1]).strip_prefix("/")?.to_owned();
         println!("request: {message}");
 
-        if method == "GET" {
+        if &path == "events" {
+            println!("Connection from socket");
+            handle_refresh_connection(&mut writer);
+        } else if method == "GET" {
             println!("path: {:?}", path);
-            if let Ok(contents) = fs::read_to_string(path) {
+            if let Ok(mut contents) = fs::read_to_string(&path) {
                 println!("Response: {contents}");
+                if path.extension().is_some_and(|ext| ext == "html") {
+                    contents = inject_socket_connection(contents);
+                }
                 send_message(&mut writer, "HTTP/1.1 200 OK", &contents, "text/html")?;
             } else {
                 eprintln!("Failed to find file");
                 send_message(&mut writer, "HTTP/1.1 404 File Not Found", "", "text/html")?;
             }
         }
+    }
+}
+
+fn handle_refresh_connection(writer: &mut BufWriter<&TcpStream>) {
+    let message = "\
+    HTTP/1.1 200 OK\r\n\
+    Content-Type: text/event-stream\r\n\
+    Cache-Control: no-cache\r\n\
+    Connection: Keep-alive\r\n\
+    \r\n";
+    let _ = writer.write_all(message.as_bytes());
+    let _ = writer.flush();
+    loop {
+        let _ = writer.write_all(b"data: Hello world!\r\n\r\n");
+        println!("sent");
+        let _ = writer.flush();
     }
 }
 
@@ -91,4 +113,26 @@ fn send_message(
     writer.write_all(message.as_bytes())?;
     writer.flush()?;
     Ok(())
+}
+
+const TO_INSERT: &str = r#"<script>
+    const events = new EventSource("http://localhost:5050/events");
+    events.onmessage = (event) => {
+        console.log("Server: ", event.data);
+    }
+</script>"#;
+
+fn inject_socket_connection(html: String) -> String {
+    let mut html = html.to_lowercase();
+    let insert_index = if let Some(i) = html.find("script") {
+        i - 1
+    } else if let Some(i) = html.find("head") {
+        i + "head".len() + 1
+    } else if let Some(i) = html.find("body") {
+        i + "body".len() + 1
+    } else {
+        return html;
+    };
+    html.insert_str(insert_index, TO_INSERT);
+    html
 }
